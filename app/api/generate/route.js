3a550313070b0137df9fbb6e0c92702720a8ee17
ahmes134 <ahmes134@mcmaster.ca>
2024-08-28 22:46:21 -0400

    L

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -20,27 +20,26 @@ You should return in the following JSON format:
 // creates a new OpenAI client instance 
 // and extracts the text data from the request body.
 export async function POST(req) {
-    const openai = new OpenAI({
-      apiKey: process.env.OPENAI_API_KEY,
-      dangerouslyAllowBrowser: true,
-    });
-    const data = await req.text()
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+  });
+  const data = await req.text()
 
-    // OpenAI API call here
-    // Creates a chat completion request to the OpenAI API
-    const completion = await openai.chat.completions.create({ 
-        messages: [ //`messages` array includes two elements
-          { role: 'system', content: systemPrompt }, // instructs the AI on how to create flashcards
-          { role: 'user', content: data }, // A ‘user’ message containing the input text from the request body
-        ],
-        model: 'gpt-4o',
-        response_format: { type: 'json_object' }, // ‘json_object’ to ensure we receive a JSON response
-      });
-    
-      // Process the API response
-      // Parse the JSON response from the API response using `JSON.parse()`
-      const flashcards = JSON.parse(completion.choices[0].message.content)
-      // Return the flashcards as a JSON response using `NextResponse.json()`
-      return NextResponse.json(flashcards.flashcards)
+  // OpenAI API call here
+  // Creates a chat completion request to the OpenAI API
+  const completion = await openai.chat.completions.create({
+    messages: [ //`messages` array includes two elements
+      { role: 'system', content: systemPrompt }, // instructs the AI on how to create flashcards
+      { role: 'user', content: data }, // A ‘user’ message containing the input text from the request body
+    ],
+    model: 'gpt-4o',
+    response_format: { type: 'json_object' }, // ‘json_object’ to ensure we receive a JSON response
+  });
 
-  }
+  // Process the API response
+  // Parse the JSON response from the API response using `JSON.parse()`
+  const flashcards = JSON.parse(completion.choices[0].message.content)
+  // Return the flashcards as a JSON response using `NextResponse.json()`
+  return NextResponse.json(flashcards.flashcards)
+
+}
